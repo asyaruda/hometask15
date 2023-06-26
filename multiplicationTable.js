aec4eaceb4e22e3e1a3eded84114f05fa894e3bc
Anastasiia Kholod <asyaruda06@gmail.com>
2023-06-27 00:28:23 +0300

    done

---
+++ multiplicationTable.js
@@ -0,0 +1,77 @@
+/*const tableContainer = document.getElementById('multiplication-table')
+
+const table = document.createElement('table')
+table.classList.add('table')
+
+const thead = document.createElement('thead')
+const headerRow = document.createElement('tr')
+
+for (let i = 1; i <= 10; i++) {
+  const th = document.createElement('th')
+  th.textContent = i
+  headerRow.appendChild(th)
+}
+
+thead.appendChild(headerRow)
+table.appendChild(thead)
+
+const tbody = document.createElement('tbody')
+
+for (let i = 1; i <= 10; i++) {
+  const row = document.createElement('tr')
+
+  for (let j = 1; j <= 10; j++) {
+    const cell = document.createElement('td')
+    cell.textContent = i * j
+    row.appendChild(cell)
+  }
+
+  tbody.appendChild(row)
+}
+
+table.appendChild(tbody)
+
+tableContainer.appendChild(table)*/
+
+
+const tableContainer = document.getElementById('multiplication-table')
+
+const table = document.createElement('table')
+table.classList.add('table')
+
+const thead = document.createElement('thead')
+const headerRow = document.createElement('tr')
+
+const emptyHeaderCell = document.createElement('th')
+headerRow.appendChild(emptyHeaderCell)
+
+for (let i = 1; i <= 10; i++) {
+  const th = document.createElement('th')
+  th.textContent = i
+  headerRow.appendChild(th)
+}
+
+thead.appendChild(headerRow)
+table.appendChild(thead)
+
+const tbody = document.createElement('tbody')
+
+for ( i = 1; i <= 10; i++) {
+  const row = document.createElement('tr')
+
+  const verticalHeaderCell = document.createElement('th')
+  verticalHeaderCell.textContent = i
+  row.appendChild(verticalHeaderCell)
+
+  for (let j = 1; j <= 10; j++) {
+    const cell = document.createElement('td')
+    cell.textContent = i * j
+    row.appendChild(cell)
+  }
+
+  tbody.appendChild(row)
+}
+
+table.appendChild(tbody)
+
+tableContainer.appendChild(table)
